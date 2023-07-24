3ca5d16805d836ceb06027a774dfb566defb36a0
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-07-24 11:54:39 -0400

    Fix default file set

--- Assets/Scenes/MainMenu/MainMenuDropdown.cs
+++ Assets/Scenes/MainMenu/MainMenuDropdown.cs
@@ -29,7 +29,7 @@ public class MainMenuDropdown : MonoBehaviour
         this.GetComponent<TMPro.TMP_Dropdown>().onValueChanged.AddListener(delegate {
             this.SetSelectedFile();
         });
-        this.SetSelectedFile(this.configFiles[0]);
+        this.SetSelectedFile();
     }
 
     /// <summary>
